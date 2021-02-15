d239c92cc4b0a5fbc791ee449c879c20d17592d2
Ryan Zemlin <rzemlin@gmail.com>
2021-02-15 23:39:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,12 @@
 def prime?(num)
-  
+  if num <= 1 || num == 0 || num == 1
+    return false
+  elsif
+    (2..num - 1).each do |i|
+      if num % i == 0
+        return false
+      end
+    end
+  end
+  true
 end
-  
-  # Add  code here!
